4a6fe1a38c38bbeacaa23783f9be20d8eff1880c
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-10-01 17:53:27 +0000

    task 0 updates

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - writes a character to stdout
+ * @c: character to print
  *
- * Return: On success 1, on error -1
+ * Return: 1 on success, -1 on error
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
+
