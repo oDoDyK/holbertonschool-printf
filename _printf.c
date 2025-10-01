98b53cba929cd223df9a4b23a962013d03dd4ce9
Rinadfahadz <rinad-alzuaybir@outlook.com>
2025-10-01 17:44:57 +0000

    task 0 updates

--- _printf.c
+++ _printf.c
@@ -1,48 +1,39 @@
-#include "main.h"
-
-/**
- * _printf - produces output according to a format
- * @format: format string
- *
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
+while (format && format[i])
 {
-	va_list args;
-	int i = 0, count = 0;
+    if (format[i] == '%')
+    {
+        i++;
+        if (!format[i])  /* if '%' is last character */
+        {
+            va_end(args);
+            return (-1);
+        }
 
-	if (!format)
-		return (-1);
-
-	va_start(args, format);
-
-	while (format && format[i])
-	{
-		if (format[i] == '%' && format[i + 1])
-		{
-			i++;
-			if (format[i] == 'd' || format[i] == 'i')
-				count += print_int(args);
-			else if (format[i] == 'c')
-				count += _putchar(va_arg(args, int));
-			else if (format[i] == 's')
-			{
-				char *str = va_arg(args, char *);
-
-				if (!str)
-					str = "(null)";
-				while (*str)
-					count += _putchar(*str++);
-			}
-			else
-				count += _putchar(format[i]);
-		}
-		else
-			count += _putchar(format[i]);
-		i++;
-	}
-
-	va_end(args);
-	return (count);
+        if (format[i] == 'c')
+            count += _putchar(va_arg(args, int));
+        else if (format[i] == 's')
+        {
+            str = va_arg(args, char *);
+            if (!str)
+                str = "(null)";
+            while (*str)
+            {
+                _putchar(*str++);
+                count++;
+            }
+        }
+        else if (format[i] == '%')
+            count += _putchar('%');
+        else
+        {
+            count += _putchar('%');
+            count += _putchar(format[i]);
+        }
+    }
+    else
+    {
+        count += _putchar(format[i]);
+    }
+    i++;
 }
 
