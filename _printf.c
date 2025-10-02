cb1546f407434895364ab8e3e55dee3552df8bc9
“Mohammed <4thkiller2005@gmail.com>
2025-10-02 17:04:18 +0000

    update

--- _printf.c
+++ _printf.c
@@ -1,65 +1,44 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
- * handle_format - Handle a format specifier and print accordingly
- * @format_char: The format specifier character
- * @args: va_list of arguments
- * Return: Number of characters printed
- */
-int handle_format(char format_char, va_list args)
-{
-	if (format_char == 'c')
-		return (print_char(args));
-	if (format_char == 's')
-		return (print_string(args));
-	/*--*/
-	if (format_char == 'd' || format_char == 'i')
-        return (print_int(args));
-	
-	if (format_char == '%')
-		return (write(1, "%", 1));
-
-	/* Unknown specifier: print '%' and the character */
-	write(1, "%", 1);
-	return (write(1, &format_char, 1) + 1);
-}
-
-/**
- * _printf - Produces output according to a format
- * @format: Format string
- * Return: Number of characters printed
+ * _printf - custom printf handling %d and %i
+ * @format: format string
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, printed = 0, res;
+    va_list args;
+    int i = 0, count = 0;
 
-	if (format == NULL)
-		return (-1);
+    if (!format)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			res = handle_format(format[i], args);
-			printed += res;
-		}
-		else
-		{
-			printed += write(1, &format[i], 1);
-		}
-		i++;
-	}
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'd' || format[i] == 'i')
+                count += print_number((long int)va_arg(args, int));
+            else if (format[i])  /* print unsupported specifier literally */
+            {
+                count += _putchar('%');
+                count += _putchar(format[i]);
+            }
+            else
+                return (-1); /* dangling % at end */
+        }
+        else
+        {
+            count += _putchar(format[i]);
+        }
+        i++;
+    }
 
-	va_end(args);
-	return (printed);
+    va_end(args);
+    return (count);
 }
 
