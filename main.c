2365cc53a7722729be938cdbf40033a6706b3a3b
“Mohammed <4thkiller2005@gmail.com>
2025-10-04 19:45:31 +0000

    update

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Hello, world!\n");
+    _printf("Unsigned: %u\n", 123456);
+    _printf("Octal: %o\n", 255);
+    _printf("Hex lowercase: %x\n", 255);
+    _printf("Hex uppercase: %X\n", 255);
+    return (0);
+}
