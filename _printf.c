4e5b343ed4333ea5bbb912487844ce9265b66778
“Mohammed <4thkiller2005@gmail.com>
2025-10-04 19:40:12 +0000

    update

--- _printf.c
+++ _printf.c
@@ -11,6 +11,8 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0, count = 0;
 	int (*func)(va_list);
+	char buffer[1024];
+	int buf_index = 0;
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
@@ -23,24 +25,50 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == '\0')
-				return (-1);
+				break;
 
 			func = get_func(format[i]);
 			if (func)
+			{
+				/* Flush buffer before calling function that uses _putchar */
+				if (buf_index > 0)
+				{
+					write(1, buffer, buf_index);
+					count += buf_index;
+					buf_index = 0;
+				}
 				count += func(args);
+			}
 			else
 			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
+				if (buf_index >= 1024)
+				{
+					write(1, buffer, buf_index);
+					count += buf_index;
+					buf_index = 0;
+				}
+				buffer[buf_index++] = '%';
+				buffer[buf_index++] = format[i];
 			}
 		}
 		else
 		{
-			_putchar(format[i]);
-			count++;
+			if (buf_index >= 1024)
+			{
+				write(1, buffer, buf_index);
+				count += buf_index;
+				buf_index = 0;
+			}
+			buffer[buf_index++] = format[i];
 		}
 		i++;
+	}
+
+	/* Flush remaining buffer */
+	if (buf_index > 0)
+	{
+		write(1, buffer, buf_index);
+		count += buf_index;
 	}
 
 	va_end(args);
