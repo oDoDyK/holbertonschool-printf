fc8c297d548ff8e46ee58159dbf5d56db6574231
“Mohammed <4thkiller2005@gmail.com>
2025-10-04 19:42:43 +0000

    update

--- _printf.c
+++ _printf.c
@@ -1,7 +1,28 @@
 #include "main.h"
 
+/* Define buffer size */
+#define BUF_SIZE 1024
+
 /**
- * _printf - produces output according to a format
+ * flush_buffer - writes the contents of buffer to stdout
+ * @buffer: buffer array
+ * @buf_index: number of chars currently in buffer
+ * Return: number of characters written
+ */
+static int flush_buffer(char *buffer, int *buf_index)
+{
+	int count = 0;
+
+	if (*buf_index > 0)
+	{
+		count = write(1, buffer, *buf_index);
+		*buf_index = 0;
+	}
+	return (count);
+}
+
+/**
+ * _printf - produces output according to a format string
  * @format: format string
  *
  * Return: number of characters printed
@@ -11,7 +32,7 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i = 0, count = 0;
 	int (*func)(va_list);
-	char buffer[1024];
+	char buffer[BUF_SIZE];
 	int buf_index = 0;
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
@@ -19,57 +40,42 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	while (format && format[i])
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == '\0')
+			if (!format[i])
 				break;
 
 			func = get_func(format[i]);
 			if (func)
 			{
-				/* Flush buffer before calling function that uses _putchar */
-				if (buf_index > 0)
-				{
-					write(1, buffer, buf_index);
-					count += buf_index;
-					buf_index = 0;
-				}
+				/* Flush buffer before calling a function that uses _putchar */
+				count += flush_buffer(buffer, &buf_index);
 				count += func(args);
 			}
 			else
 			{
-				if (buf_index >= 1024)
-				{
-					write(1, buffer, buf_index);
-					count += buf_index;
-					buf_index = 0;
-				}
+				/* If unknown specifier, buffer '%' and character */
+				if (buf_index >= BUF_SIZE - 2)
+					count += flush_buffer(buffer, &buf_index);
 				buffer[buf_index++] = '%';
 				buffer[buf_index++] = format[i];
 			}
 		}
 		else
 		{
-			if (buf_index >= 1024)
-			{
-				write(1, buffer, buf_index);
-				count += buf_index;
-				buf_index = 0;
-			}
+			/* Add normal character to buffer */
+			if (buf_index >= BUF_SIZE)
+				count += flush_buffer(buffer, &buf_index);
 			buffer[buf_index++] = format[i];
 		}
 		i++;
 	}
 
-	/* Flush remaining buffer */
-	if (buf_index > 0)
-	{
-		write(1, buffer, buf_index);
-		count += buf_index;
-	}
+	/* Flush remaining characters */
+	count += flush_buffer(buffer, &buf_index);
 
 	va_end(args);
 	return (count);
